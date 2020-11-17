5cb388bee2e9850513dc20cad65a31ee5ba30412
JoaoMaieron <71529709+JoaoMaieron@users.noreply.github.com>
2020-11-17 14:45:10 -0300

    Update instrument.py

--- src/instrument.py
+++ src/instrument.py
@@ -5,6 +5,6 @@ class Instrument:
     and other instrument properties
     """
     def __init__(self, name, midi_number, note_stream = []):
-        self.name = name
-        self.midi_number = midi_number
-        self.note_stream = note_stream
+        self.name = name """ Name of the instrument """
+        self.midi_number = midi_number """ Number of the midi correspondent to that instrument """
+        self.note_stream = note_stream """ Channel of the instrument """
