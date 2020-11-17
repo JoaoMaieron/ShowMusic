539f5db8d1b73e8230914bafbee82f2eff30e339
JoaoMaieron <71529709+JoaoMaieron@users.noreply.github.com>
2020-11-17 14:27:08 -0300

    Update music.py

--- src/music.py
+++ src/music.py
@@ -5,6 +5,11 @@ class Music:
     Class that manage the channels, instruments,
         notes and composes the music in general
     """
+    
+    def printMusic():
+        """ Prints the music to save it in a file """
+        pass
+    
     def __init__(self, pygame_player, name = '',  active_channels=[], bpm=100):
         self.pygame_player = pygame_player
         self.name = name
