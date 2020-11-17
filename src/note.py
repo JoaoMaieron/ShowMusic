99cb6fd9a440f949b2458288bb9fafe0fd16fa8e
JoaoMaieron <71529709+JoaoMaieron@users.noreply.github.com>
2020-11-17 14:43:36 -0300

    Update note.py

--- src/note.py
+++ src/note.py
@@ -3,12 +3,22 @@ from pygame import midi
 class Note:
     """ Class of musical notes """
     def __init__(self, name, note_number, octave=4):
-        self.name = name
-        self.note_number = note_number
-        self.octave = octave
+        self.name = name  """ Name of the note """
+        self.midi_number = note_number """ Midi number of the note """
+        self.octave = octave """ Current octave of the note """
 
     def __str__(self):
         string = ('Name(name=' + str(self.name) + '), '
                   +'Number(number=' + str(self.note_number) + '), '
                   +'Octave(octave=' + str(self.octave) +')' )
         return string
+    
+    def increaseOctave():
+        """ Increase the octave of the note """
+        pass
+    
+    def decreaseOctave():
+        """ Decrease the octave of the note """
+        pass
+    
+        
